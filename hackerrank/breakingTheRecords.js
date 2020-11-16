f788fbb6e58aaa851522a3e9ca5d29db5bcd618a
Peter Kang <peeterkang@gmail.com>
2020-11-16 12:19:23 -0800

    solved breaking the records algo on hackerrank

---
+++ hackerrank/breakingTheRecords.js
@@ -0,0 +1,35 @@
+/* 
+Maria plays college basketball and wants to go pro. Each season she maintains a record of her play. She tabulates the number of times she breaks her season record for most points and least points in a game. Points scored in the first game establish her record for the season, and she begins counting from there.
+
+For example, assume her scores for the season are represented in the array . Scores are in the same order as the games played. She would tabulate her results as follows:
+
+                                 Count
+Game  Score  Minimum  Maximum   Min Max
+ 0      12     12       12       0   0
+ 1      24     12       24       0   1
+ 2      10     10       24       1   1
+ 3      24     10       24       1   1
+Given the scores for a season, find and print the number of times Maria breaks her records for most and least points scored during the season.
+*/
+
+const scores = [10, 5, 20, 20, 4, 5, 2, 25, 1];
+// [2, 4]
+
+function breakingRecords(scores) {
+    let [high, low] = [scores[0], scores[0]];
+    let [max, min] = [0, 0];
+
+    for (let i = 0; i < scores.length; i++) {
+        if (scores[i] > high) {
+            high = scores[i];
+            max++;
+        }
+        if (scores[i] < low) {
+            low = scores[i];
+            min++;
+        }
+    }
+    return [max, min];
+}
+
+console.log(breakingRecords(scores));
